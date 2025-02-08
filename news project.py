cffacfbb64c277d69c59d3960c72b4b313ddb59a
unknown <ishaanagarwal1805@gmail.com>
2025-02-08 12:59:55 +0530

    Make call to opensource news api

--- news project.py
+++ news project.py
@@ -19,24 +19,38 @@ def summarise(contents: list)->str:
     combined_content = "\n".join(contents)
     try:
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-4-turbo",
             messages=[
                 {"role": "system", "content": "You are an expert in detecting and removing bias in articles"},
                 {"role": "user", "content": f"Summarize the following multiple news articles into a single unbiased report. Don't create any new information or assumptions on your own. Ensure that all the facts are present: {combined_content}"}
             ]
         )
-        print(response)
         return response.choices[0].message.content
     except Exception as e:
         print(f"Error calling OpenAI API: {e}")
         return "Summary unavailable."
 
+def get_posts():
+    combined_content=[]
+    api_key='api_live_DhnggeJIBMu06ak83Zog3J8yzpwe3rAGuu8ud0IqT8Akh'
+    sites=['cnn.com','ndtv.com','nytimes.com','hindustantimes.com']
+    url='https://api.apitube.io/v1/news/top-headlines?api_key='+api_key+'&per_page=5&page=1&source.domain='
+    for site in sites:
+        response = requests.post(url+site)
+        if response.status_code == 200:
+            posts = response.json()
+            for res in posts["results"]:
+                combined_content.append(res["body"])
+        else:
+            continue
+    
+    return combined_content
+
+
 def job():
     print("chala")
-    combined_content = ["The x party always does this, 3 people died", "The x party is innocent, only 3 people died", "Cricketer takes 4 wickets"]
-    # for url in sources:
-    #     #parse the content
-    #     combined_content.append(response.json().get("description"))
+    
+    combined_content = get_posts()
     
     embeddings = embedding_model.encode(combined_content)
     clusters=[]
@@ -44,8 +58,6 @@ def job():
     for i, content in enumerate(combined_content):
         added = False
         for cluster in clusters:
-            print(cluster["index"])
-            print(type(cluster["index"]))
             similarity = np.dot(embeddings[i], embeddings[cluster["index"]]) / (np.linalg.norm(embeddings[i]) * np.linalg.norm(embeddings[cluster["index"]]))
             if similarity > 0.8:  # Threshold for merging articles
                 cluster["content"]+content
@@ -54,7 +66,6 @@ def job():
 
         if added == False:
             clusters.append({"index": i, "content": content})
-            print(type(clusters))
     
     for cluster in clusters:
         summary = summarise(cluster["content"])
@@ -65,7 +76,7 @@ def job():
     
 
 
-schedule.every().day.at("19:51").do(job)
+schedule.every().day.at("12:56").do(job)
 
 while 1:
     schedule.run_pending()
