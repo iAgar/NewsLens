49a13c7419e6a3b90ae60051b5fd4638b3a323c2
unknown <ishaanagarwal1805@gmail.com>
2025-02-15 13:37:30 +0530

    Add axios to react page for making successful API calls

--- news_frontend/src/App.js
+++ news_frontend/src/App.js
@@ -4,9 +4,22 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 function App() {
+  const [data, setData] = useState('');
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5000/') 
+            .then(response => {
+                setData(response.data.message);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
   var settings = {
     dots: true,
     infinite: true,
